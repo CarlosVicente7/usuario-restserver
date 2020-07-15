c59c95aed7848f54ddd0e663aeaa6bef32ec9736
Carlos Vicente <cvicenteyriarte@gmail.com>
2020-07-15 15:33:35 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 //PUERTO
 //==========
 process.env.PORT = process.env.PORT || 3000;
+
+
+//=============
+//ENTORNO
+//=============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=============
+//BD
+//============= 
+let urlBD;
+if (process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe';
+} else {
+    urlBD = 'mongodb+srv://cvicenteadmin:ntKxhoeyynEc3Sgu@cluster0.tcwsg.mongodb.net/cafe';
+}
+process.env.URLBD = urlBD;
